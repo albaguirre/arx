b39c1fde45a2747b6aaeaa05aeb83a1512489ace
James W. Mills <jameswmills@ti.com>
2012-10-01 11:30:48 -0500

    Initial public commit
    Signed-off-by: James W. Mills <jameswmills@ti.com>

---
+++ source/apps/ARCowboid/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := ARCowboid
+LOCAL_JNI_SHARED_LIBRARIES := libarcamera_jni
+LOCAL_REQUIRED_MODULES := libarcamera_jni
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under, $(LOCAL_PATH))
---
+++ source/apps/ARXStreams/Android.mk
@@ -0,0 +1,10 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_PACKAGE_NAME := ARXStreams
+LOCAL_JNI_SHARED_LIBRARIES := libarxstreams_jni
+LOCAL_REQUIRED_MODULES := libarxstreams_jni
+include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under, $(LOCAL_PATH))
