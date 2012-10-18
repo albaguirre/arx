8fdf0a30cd77e53031b294a865a8086443e63916
Alberto Aguirre <a-aguirre@ti.com>
2012-10-18 10:46:16 -0500

    ARX apps: Add proguard configuration
    Add proguard configuration to prevent of context
    field and callback methods which are used by the jni
    native context.
    
    Change-Id: I22ded8e384236a510d192c0ae895c2dc8db6d12d
    Signed-off-by: Alberto Aguirre <a-aguirre@ti.com>

--- source/apps/ARCowboid/Android.mk
+++ source/apps/ARCowboid/Android.mk
@@ -7,6 +7,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := ARCowboid
 LOCAL_JNI_SHARED_LIBRARIES := libarcamera_jni
 LOCAL_REQUIRED_MODULES := libarcamera_jni
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under, $(LOCAL_PATH))
--- source/apps/ARXStreams/Android.mk
+++ source/apps/ARXStreams/Android.mk
@@ -5,6 +5,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := ARXStreams
 LOCAL_JNI_SHARED_LIBRARIES := libarxstreams_jni
 LOCAL_REQUIRED_MODULES := libarxstreams_jni
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under, $(LOCAL_PATH))
